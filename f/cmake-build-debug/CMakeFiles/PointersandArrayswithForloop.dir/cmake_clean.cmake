c9d87868eb57388caa275abf044f903de9f2e15f
Viktor Madarasz <viktormadarasz@users.noreply.github.com>
2018-01-06 13:41:23 +0100

    Add files via upload

---
+++ f/cmake-build-debug/CMakeFiles/PointersandArrayswithForloop.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PointersandArrayswithForloop.dir/main.cpp.o"
+  "PointersandArrayswithForloop.pdb"
+  "PointersandArrayswithForloop"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PointersandArrayswithForloop.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
